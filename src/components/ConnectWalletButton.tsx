b185568d20277fee2f40be3c9af0e0d3bca5ac18
Nikkrana28 <60419615+Nikkrana28@users.noreply.github.com>
2021-10-10 13:59:18 +0530

    update

---
+++ src/components/ConnectWalletButton.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useWalletModal } from '@pancakeswap/uikit'
+import useAuth from 'hooks/useAuth'
+import { useTranslation } from 'contexts/Localization'
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: #28B463;
+  cursor: pointer;
+  border:none;
+  outline: none;
+  text-align: center;
+  font-weight: 300;
+  border-radius: 30px;
+  padding: 10px 24px;
+  text-size: 50px;
+  color: white;
+  width: 100%;
+  font-size : 17px;
+  text-decoration: none;
+`;
+
+
+
+const ConnectWalletButton = (props) => {
+  const { t } = useTranslation()
+  const { login, logout } = useAuth()
+  const { onPresentConnectModal } = useWalletModal(login, logout, t)
+
+  return (
+    <Button onClick={onPresentConnectModal} {...props}>
+      {t('Connect Wallet')}
+    </Button>
+  )
+}
+
+export default ConnectWalletButton
